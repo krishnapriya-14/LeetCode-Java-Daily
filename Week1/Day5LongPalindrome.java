138f946fd2b5fc7da366913cdebe3d2a8e87b230
Krishna Priya Kommineni <96191562+krishnapriya-14@users.noreply.github.com>
2025-10-27 00:54:51 -0500

    Create Day5LongPalindrome.java

---
+++ Week1/Day5LongPalindrome.java
@@ -0,0 +1,27 @@
+//Day 5 — #5 Longest Palindromic Substring (Expand around center)
+//Concepts: every palindrome has a center (odd/even), expand while equal.
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+
+    }
+        private int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--; right++;
+        }
+        return right - left - 1;
+
+        
+    }
+}
